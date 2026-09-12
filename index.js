e6c96ebaeae7cd90c83e99aad26cc194380e2931
vtl-devops <rahul@vensysco.in>
2026-09-12 08:16:38 +0000

    inital commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from	'express'
+
+const app	= express()
+const PORT = process.env.PORT ?? 8080
+
+app.get('/', (req, res) => {
+	return res.json({
+		msg : 'Hello from the server'
+	})
+})
+
+app.listen(PORT, () => {
+	console.log('Server is Up and Running on PORT 8080')
+})
