65400fd38675bbe5494fd31ed074fd41ac5e2966
vtl-devops <rahul@vensysco.in>
2026-09-12 09:02:00 +0000

    update the code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT ?? 8080
 
 app.get('/', (req, res) => {
 	return res.json({
-		msg : 'Hello from the server'
+		msg : 'Hello from the server, yoo>>> github actions s working...'
 	})
 })
 
